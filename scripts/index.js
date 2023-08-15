04de3ca68367b7dbca5771ee373862ff9a92e744
Evgenia Ermakova <mikassik@yandex.ru>
2023-08-15 03:01:00 +0300

    popups_finished

--- scripts/index.js
+++ scripts/index.js
@@ -74,27 +74,27 @@ closeButtons.forEach((item) => {
   const items = [
     {
       name: 'Карачаевск',
-      link: 'images/karachayevsk.png'
+      link: 'https://images.unsplash.com/photo-1588584922681-745a2223f72c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8JUQwJUJBJUQwJUIwJUQxJTgwJUQwJUIwJUQxJTg3JUQwJUIwJUQwJUI1JUQwJUIyJUQxJTgxJUQwJUJBfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
     },
     {
       name: 'Гора Эльбрус',
-      link: 'images/gora_elbrus.png'
+      link: 'https://wikiway.com/upload/resize_cache/iblock/902/500_330_2/Elbrus-na-zakate.jpg'
     },
     {
       name: 'Домбай',
-      link: 'images/dombai.png'
+      link: 'https://turvopros.com/wp-content/uploads/2022/10/dombai-kanatka.jpg'
     },
     {
       name: 'Рускеала',
-      link: 'images/Ruskeala.png'
+      link: 'https://avatars.mds.yandex.net/get-altay/1371862/2a000001655d39498bbc93fbabd8e526d6e0/XXXL'
     },
     {
       name: 'Камчатка',
-      link: 'images/Kamchatka.png'
+      link: 'https://avatars.dzeninfra.ru/get-zen_doc/1921148/pub_5ee9a9472ab5e326049535f2_5ee9aa19987ef673f1bd01af/scale_1200'
     },
     {
       name: 'Владивосток',
-      link: 'images/vladivostok.png'
+      link: 'https://webpulse.imgsmail.ru/imgpreview?mb=webpulse&key=pulse_cabinet-image-5f590f63-ade4-421d-ac35-48e328c50e7f'
     }
   ];   
 
