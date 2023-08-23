89ba0c7350b4012aa152cb501feb628d0876b785
Evgenia_Ermakova <128416013+Jennimikka@users.noreply.github.com>
2023-08-23 10:00:05 +0300

    6spr

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,47 +1,50 @@
-const enableValidation = function(validateSetting) {
-    const setButtonForm = (input) => {
-        const form = input.closest('.popup__info');
-        const btnElement = form.querySelector('.popup__button');
-        if (form.checkValidity()) {
-            btnElement.classList.remove(validateSetting.inactiveButtonClass)
-            btnElement.removeAttribute('disabled');
-        } else {
-            btnElement.classList.add(validateSetting.inactiveButtonClass)
-            btnElement.setAttribute('disabled', true);
-        }
-    };
+let validateSetting = {};
 
-    // Находим элемент ошибки внутри самой функции
-    const showInputError = (input) => {
-        input.classList.add(validateSetting.inputErrorClass);
-        input.nextElementSibling.textContent = input.validationMessage;    
+const setButtonForm = (input) => {
+    const form = input.closest(validateSetting.formSelector);
+    const btnElement = form.querySelector(validateSetting.submitButtonSelector);
+    if (form.checkValidity()) {
+        btnElement.classList.remove(validateSetting.inactiveButtonClass)
+        btnElement.removeAttribute('disabled');
+    } else {
+        btnElement.classList.add(validateSetting.inactiveButtonClass)
+        btnElement.setAttribute('disabled', true);
     }
+};
 
-    // находим элемент ошибки
-    const hideInputError = (input) => {
-        input.classList.remove(validateSetting.inputErrorClass);
-        input.nextElementSibling.textContent = ''; 
-    }
+// Находим элемент ошибки внутри самой функции
+const showInputError = (input) => {
+    input.classList.add(validateSetting.inputErrorClass);
+    input.nextElementSibling.textContent = input.validationMessage;    
+}
 
-    const checkInputValidity = (input) => {
-        if (!input.validity.valid) {
-            showInputError(input);
-        } else {
-            hideInputError(input);
-        }
+// находим элемент ошибки
+const hideInputError = (input) => {
+    input.classList.remove(validateSetting.inputErrorClass);
+    input.nextElementSibling.textContent = ''; 
+}
+
+const checkInputValidity = (input) => {
+    if (!input.validity.valid) {
+        showInputError(input);
+    } else {
+        hideInputError(input);
     }
-    // устанавливаем прослушиватель событий
-    const setEventListeners = (form) => {
-        const inputList = Array.from(form.querySelectorAll(validateSetting.inputSelector))
-        const btnElement = form.querySelector(validateSetting.submitButtonSelector)
-        inputList.forEach((input) => {
-            input.addEventListener('input', () => {
-                checkInputValidity(input);
-                setButtonForm(input)
-            });
+}
+// устанавливаем прослушиватель событий
+const setEventListeners = (form) => {
+    const inputList = Array.from(form.querySelectorAll(validateSetting.inputSelector))
+    const btnElement = form.querySelector(validateSetting.submitButtonSelector)
+    inputList.forEach((input) => {
+        input.addEventListener('input', () => {
+            checkInputValidity(input);
+            setButtonForm(input)
         });
-    };
+    });
+};
 
+const enableValidation = function(setting) {
+    validateSetting = setting;
     const formList = Array.from(
         document.querySelectorAll(validateSetting.formSelector)
     );
